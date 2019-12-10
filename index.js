74c36c1d761100bf99d8a154348152da5d7379d9
JuanCuestaV <juanbryanc@gmail.com>
2019-12-10 15:20:35 +0100

    Agregado solucion prueba. Chat, subida de archivos, escribiendo, foto identificadora

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const path = require("path");
 const socketIO = require("socket.io");
+const fileUpload = require('express-fileupload');
 
 // Inicializar
 const app = express();
+app.use(fileUpload());
 app.set("port", process.env.PORT || 3000);
 
 const server = app.listen(app.get("port"), () => {
-  console.log("Servidor escuchando en puerto: 3000");
+    console.log("Servidor escuchando en puerto: 3000");
 });
 
 const io = socketIO(server);
@@ -17,21 +19,48 @@ app.use(express.static(path.join(__dirname, "public")));
 
 //Socket
 io.on("connection", socket => {
-  //console.log("Nueva conexion");
-  socket.on("unido", data => {
-    io.sockets.emit("unido", data);
-  });
+    //console.log("Nueva conexion");
+    socket.on("unido", data => {
+        io.sockets.emit("unido", data);
+    });
 
-  socket.on("texto", data => {
-    io.sockets.emit("texto", data);
-  });
+    socket.on("texto", data => {
+        io.sockets.emit("texto", data);
+    });
 
-  socket.on("escribiendo", data => {
-    socket.broadcast.emit("escribiendo", data);
-  });
+    socket.on("escribiendo", data => {
+        socket.broadcast.emit("escribiendo", data);
+    });
 
-  socket.on("archivo", data => {
-    console.log(data);
-  });
+    socket.on("archivo", data => {
+        console.log(data);
+    });
+
+    socket.on("file", data => {
+        io.sockets.emit("file", data);
+    });
+
+    app.post('/subir', (req, res) => {
+        let EDFile = req.files.file
+        EDFile.mv(`./public/upload/${EDFile.name}`, err => {
+            if (err) return res.status(500).send({ message: err })
+            return res.status(200).redirect('index.html');
+        });
+    });
+
+    app.post('/subirfile', (req, res) => {
+        let EDFile = req.files.file
+        EDFile.mv(`./public/subirfile/${EDFile.name}`, err => {
+            if (err) return res.status(500).send({ message: err })
+            console.log(EDFile.name);
+            io.sockets.emit('file', EDFile.name);
+            return res.status(200); //.redirect('index.html');
+        });
+    });
+
+    app.get('/', (req, res) => {
+        res.send('./public/index.html');
+
+    });
 
 });
--- public/js/index.js
+++ public/js/index.js
@@ -7,35 +7,54 @@ let entrar = document.getElementById('entrar');
 let texto = document.getElementById('texto');
 let enviar = document.getElementById('enviar');
 let accion = document.getElementById('accion');
-let archivo = document.getElementById('archivo');
+let archivo = document.getElementById('archivochat');
+//let clickder = document.getElementById('clickder');
 
-entrar.addEventListener('click', function(){
-    socket.emit('unido', {nickname: nickname.value});
+//------------------- Eventos
+entrar.addEventListener('click', function() {
+    socket.emit('unido', { nickname: nickname.value });
 });
 
-enviar.addEventListener('click', function(){
-    socket.emit('texto', {nickname:nickname.value, texto: mensaje.value});
+enviar.addEventListener('click', function() {
+    socket.emit('texto', { nickname: nickname.value, texto: mensaje.value });
 });
 
-mensaje.addEventListener('keypress', function(){
+mensaje.addEventListener('keypress', function() {
     socket.emit('escribiendo', nickname.value);
 });
 
-archivo.addEventListener('click', function(){
+archivo.addEventListener('click', function() {
     var file = archivo[0].files[0];
     var nombre = file.name;
-    socket.emit('archivo', nombre );
+    console.log(nombre);
+    //socket.emit('archivo', nombre);
 });
 
-//-----------------------
+/*clickder.addEventListener('mousedown', function(ev) {
+    if (ev.which == 3) { alert("Right mouse button clicked on element with id myId"); }
+});*/
 
+/*clickder.addEventListener('click', function(ev) {
+    if (ev.which == 3) { 
+});*/
+
+//----------------------- ON
 socket.on('unido', (data) => {
     accion.innerHTML = data.nickname + ' se ha unido al chat';
 });
 
 socket.on("texto", (data) => {
     accion.innerHTML = "";
-    texto.innerHTML += '<p><strong>' + data.nickname + '</strong>: ' + data.texto + '</p><br>';
+    var imagen = "upload/juan.jpg";
+    if (nickname.value == "Juan") {
+        imagen = "upload/juan.jpg";
+    } else if (nickname.value == "Bryan") {
+        imagen = "upload/bryan.jpg";
+    }
+    if (nickname.value == "Jose") {
+        imagen = "upload/jose.jpg";
+    }
+    texto.innerHTML += '<img src="' + imagen + '"><p id="clickder"><strong>' + data.nickname + '</strong>: ' + data.texto + '</p><br>';
     mensaje.value = "";
 });
 
@@ -43,9 +62,24 @@ socket.on("escribiendo", data => {
     accion.innerHTML = "<strong>" + data + "</strong>" + ": <img src='img/escribiendo.gif'>"
 });
 
+socket.on("file", (data) => {
+    accion.innerHTML = "";
+    var imagen = "upload/juan.jpg";
+    if (nickname.value == "Juan") {
+        imagen = "upload/juan.jpg";
+    } else if (nickname.value == "Bryan") {
+        imagen = "upload/bryan.jpg";
+    } else
+    if (nickname.value == "Jose") {
+        imagen = "upload/jose.jpg";
+    }
+    texto.innerHTML += '<img src="' + imagen + '"><p id="clickder"><strong>' + data.nickname + '</strong>: <a href="subirfile/' + data + '">' + data + '</a></p><br>';
+    mensaje.value = "";
+});
+
 socket.on('archivo', (data) => {
     var texto = `${data.texto}`;
     $('#contenido').text(texto);
     console.log(`Archivo: ${data.nombre}`);
-    console.log(`Contenido: `+texto);
-  });
+    console.log(`Contenido: ` + texto);
+});
